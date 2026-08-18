65bb9f4d88b49c8c0faab355fbf5627117341d33
Nathalia Lyra Varela de Albuquerque <nathalialyra@ufpr.br>
2026-08-18 13:37:03 -0300

    Update 02-ddl-cliente.sql

--- scripts/sql/02-ddl-cliente.sql
+++ scripts/sql/02-ddl-cliente.sql
@@ -1,6 +1,6 @@
 --- SCHEMA CLIENTE
 
-CREATE TABLE cliente.clientes (
+CREATE TABLE IF NOT EXISTS cliente.clientes (
     cpf VARCHAR(11) PRIMARY KEY,
     nome VARCHAR(120) NOT NULL,
     email VARCHAR(120) NOT NULL UNIQUE,
@@ -14,7 +14,7 @@ CREATE TABLE cliente.clientes (
     uf CHAR(2) NOT NULL
 );
 
-CREATE TABLE cliente.solicitacoes (
+CREATE TABLE IF NOT EXISTS cliente.solicitacoes (
     cpf VARCHAR(11) PRIMARY KEY,
     nome VARCHAR(120) NOT NULL,
     email VARCHAR(120) NOT NULL UNIQUE,
@@ -33,10 +33,10 @@ CREATE TABLE cliente.solicitacoes (
 );
 
 -- criando index para facilitar a busca da solicitação por status e do cliente por nome
-CREATE INDEX ix_solicitacoes_status ON cliente.solicitacoes (status);
-CREATE INDEX ix_clientes_nome ON cliente.clientes (nome);
+CREATE INDEX IF NOT EXISTS ix_solicitacoes_status ON cliente.solicitacoes (status);
+CREATE INDEX IF NOT EXISTS ix_clientes_nome ON cliente.clientes (nome);
 
-CREATE TABLE cliente.comandos_processados (
+CREATE TABLE IF NOT EXISTS cliente.comandos_processados (
 saga_id VARCHAR(36) NOT NULL,
 tipo VARCHAR(80) NOT NULL,
 processado_em TIMESTAMP NOT NULL DEFAULT now(),
