de3d6d9fa41efc13ba36bcfd9d21a7f7af9efd20
lauraklemba <laurakcordeiro2@gmail.com>
2026-08-29 09:30:31 -0300

    test: corrige teste da home do cliente

--- frontend/src/app/features/cliente/home/home-cliente.component.spec.ts
+++ frontend/src/app/features/cliente/home/home-cliente.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { provideRouter } from '@angular/router';
 import { HomeClienteComponent } from './home-cliente.component';
 
 describe('HomeClienteComponent', () => {
@@ -9,6 +9,7 @@ describe('HomeClienteComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [HomeClienteComponent],
+      providers: [provideRouter([])]
     }).compileComponents();
 
     fixture = TestBed.createComponent(HomeClienteComponent);
