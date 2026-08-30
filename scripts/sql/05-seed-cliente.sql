82c3eeabb8e92a4d86861216f4db44ab6d9d8b2a
nvalbuquerque <nathalialyra@ufpr.br>
2026-08-30 19:21:55 -0300

    Cliente e Gerente: get cpf + admin/seed

--- scripts/sql/05-seed-cliente.sql
+++ scripts/sql/05-seed-cliente.sql
@@ -1,10 +1,3 @@
---cpf nome email senha salário dados de endereço
---12912861012 Catharyna cli1@bantads.com.br tads R$ 10.000,00 você escolhe
---09506382000 Cleuddônio cli2@bantads.com.br tads R$ 20.000,00 você escolhe
---85733854057 Catianna cli3@bantads.com.br tads R$ 3.000,00 você escolhe
---58872160006 Cutardo cli4@bantads.com.br tads R$ 500,00 você escolhe
---76179646090 Coândrya cli5@bantads.com.br tads R$ 1.500,00 você escolhe
-
 INSERT INTO cliente.clientes (cpf, nome, email, telefone, salario, logradouro, numero, complemento, cep, cidade, uf) VALUES
 ('12912861012', 'Catharyna', 'cli1@bantads.com.br', '41999999999', 10000.00, 'Rua das Flores', '123', 'Apto 101', '80000000', 'Curitiba', 'PR'),
 ('09506382000', 'Cleuddônio', 'cli2@bantads.com.br', '41998999999', 20000.00, 'Avenida Brasil', '456', 'Sala 202', '80000001', 'Curitiba', 'PR'),
