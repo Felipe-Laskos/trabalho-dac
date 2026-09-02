947ee8d9aeda1204776d885932207ccb81b262ff
nathaly <nathalymartins@ufpr.br>
2026-09-01 21:27:14 -0300

    fix(email): padronizacao das configuracoes do projeto conforme especificacao

--- ms-email/src/test/java/br/ufpr/dac/grupo2/email/MsEmailApplicationTests.java
+++ ms-email/src/test/java/br/ufpr/dac/grupo2/email/MsEmailApplicationTests.java
@@ -1,12 +1,12 @@
 package br.ufpr.dac.grupo2.email;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class MsEmailApplicationTests {
 
-	@Test
+//	@Test
 	void contextLoads() {
 	}
 
