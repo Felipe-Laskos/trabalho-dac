eba990104e2b73fc7e37d279cd3e6028ce70817f
nvalbuquerque <nathalialyra@ufpr.br>
2026-09-08 13:07:16 -0300

    fix: adiciona import do ResponseEntity em SolicitacaoREST

--- ms-cliente/src/main/java/br/ufpr/dac/grupo2/cliente/rest/SolicitacaoREST.java
+++ ms-cliente/src/main/java/br/ufpr/dac/grupo2/cliente/rest/SolicitacaoREST.java
@@ -19,6 +19,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import org.springframework.http.ResponseEntity;
+
 @CrossOrigin
 @RestController
 @RequestMapping("/solicitacoes")
