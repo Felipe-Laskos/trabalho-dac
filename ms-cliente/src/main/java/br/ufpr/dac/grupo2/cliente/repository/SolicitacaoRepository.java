c5a8611ac5851b66d65b7fb74b87984fff1b8a50
Felipe-Laskos <felipematheuslaskos@gmail.com>
2026-09-21 11:04:38 -0300

    fix: some fixes in s6

--- ms-cliente/src/main/java/br/ufpr/dac/grupo2/cliente/repository/SolicitacaoRepository.java
+++ ms-cliente/src/main/java/br/ufpr/dac/grupo2/cliente/repository/SolicitacaoRepository.java
@@ -3,12 +3,10 @@ package br.ufpr.dac.grupo2.cliente.repository;
 import br.ufpr.dac.grupo2.cliente.model.Solicitacao;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SolicitacaoRepository extends JpaRepository<Solicitacao, String> {
     List<Solicitacao> findByStatus(String status);
     boolean existsByEmail(String email);
-    Optional<Solicitacao> findByCpf(String cpf);
 }
